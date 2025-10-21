8d651d240c2645e7fa4e303c6a9e5c9678969037
Dilshada101 <dilshadafarooq1014@gmail.com>
2025-10-21 22:38:55 +0530

    implemented View and Download option

--- StudyPortal/urls.py
+++ StudyPortal/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('about/', views.about_page, name='about_page'), 
     path('feature1/', views.feature1_page, name='feature1_page'),
     path('/register/', views.register, name='register'),
-    # path('assignments/', views.assignment_dashboard, name='assignment_dashboard'),
-
+    path('view/<str:type>/<int:file_id>/', views.view_file, name='view_file'),
+    path('download/<str:type>/<int:file_id>/', views.download_file, name='download_file'),
 ]
--- StudyPortal/views.py
+++ StudyPortal/views.py
@@ -6,6 +6,10 @@ from .models import *
 from django.shortcuts import render,redirect
 from django.contrib import messages
 from StudyPortal.forms import RegistrationForm
+from django.shortcuts import get_object_or_404
+from django.http import FileResponse, HttpResponse
+import mimetypes
+import os
 
 def register(request):
     if request.method == 'POST':
@@ -92,6 +96,43 @@ def resources_dashboard(request):
 
 def progress_dashboard(request):
     return render(request, 'admin/custom_progress.html')
+
+
+
+def get_model_by_type(file_type):
+    if file_type == 'assignment':
+        return Assignment
+    elif file_type == 'note':
+        return Note
+    elif file_type == 'resource':
+        return Resource
+    else:
+        return None
+
+
+def view_file(request, type, file_id):
+    model = get_model_by_type(type)
+    obj = get_object_or_404(model, id=file_id)
+    file_path = obj.file.path
+
+    # Serve file inline for viewing
+    response = FileResponse(open(file_path, 'rb'))
+    response['Content-Disposition'] = f'inline; filename="{os.path.basename(file_path)}"'
+    return response
+
+
+def download_file(request, type, file_id):
+    model = get_model_by_type(type)
+    obj = get_object_or_404(model, id=file_id)
+    file_path = obj.file.path
+
+    # Set response for download
+    mime_type, _ = mimetypes.guess_type(file_path)
+    with open(file_path, 'rb') as f:
+        response = HttpResponse(f.read(), content_type=mime_type)
+        response['Content-Disposition'] = f'attachment; filename="{os.path.basename(file_path)}"'
+        return response
+
 
 
 
